f687448272f9145e5d380ed41ca77caa2f28cb73
Amine Smahi <mohammed.amine.smahi@gmail.com>
2017-09-04 02:37:37 -0700

    Add main class

---
+++ src/semaine3/Main.java
@@ -0,0 +1,15 @@
+package test1;
+
+import test1.Chat;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		int[] b = {1,2};
+		Chat blah = new Chat("tom",7,"black");
+		blah.setAge(20);
+		System.out.println(blah.getAge());
+		System.out.println(b[2]);
+		
+	}
+}
